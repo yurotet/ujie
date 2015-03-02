3930f9489257bb4d403daba90cb7e38fc5fb4607
shenyi <sy890622@gmail.com>
2015-03-03 00:14:28 +0800

    add constant.py; optimize verify, add try/except

--- ujieservice/wechat/api.py
+++ ujieservice/wechat/api.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse
 from tastypie import fields
 from tastypie.resources import Resource
+from ujieservice import constant
+
 
 class VerifyResource(Resource):
     class Meta:
@@ -15,26 +17,25 @@ class VerifyResource(Resource):
         # Filtering disabled for brevity...
         return self.get_object_list(bundle.request)
 
-TOKEN = '75CD9F8F7A3D452392CEEC2A9573C3B7'
-
 def hello(req):
     return HttpResponse('helloworld')
 
 def verify(req):
-    ispass = checksignature(req)
-    if ispass:
-        echostr = req.GET['echostr']
-        return HttpResponse(echostr)
-    else:
+    try:
+        ispass = checksignature(req)
+        if ispass:
+            echostr = req.REQUEST['echostr']
+            return HttpResponse(echostr)
+    except:
         return HttpResponse('error')
 
 #check signature
 def checksignature(req):
     assert isinstance(req, HttpRequest)
-    signature = req.GET['signature']
-    timestamp = req.GET['timestamp']
-    nonce = req.GET['nonce']
-    arr = [TOKEN, timestamp, nonce]
+    signature = req.REQUEST['signature']
+    timestamp = req.REQUEST['timestamp']
+    nonce = req.REQUEST['nonce']
+    arr = [constant.TOKEN, timestamp, nonce]
     arr.sort()
     tmp = ''.join(arr)
     tmp = hashlib.sha1(tmp).hexdigest()
