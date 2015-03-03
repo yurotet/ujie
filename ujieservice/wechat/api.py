620cfa9101b2bfc89c87dec0e1235bd7d368efe4
sy沈毅 <yi.shen@ctrip.com>
2015-03-03 09:51:21 +0800

    show access token

--- ujieservice/wechat/api.py
+++ ujieservice/wechat/api.py
@@ -27,7 +27,7 @@ def verify(req):
 #    "openid":"OPENID",
 #    "scope":"SCOPE"
 # }
-# https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=http%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base&state=123#wechat_redirect
+# https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe2c38ce50f1ccb58&redirect_uri=http%3A%2F%2Fwx.ujietrip.com%2Fapi%2Fauthorize&response_type=code&scope=snsapi_base&state=123#wechat_redirect
 def authorize(req):
     code = req.REQUEST.get('code', '')
     state = req.REQUEST.get('state', '')
@@ -36,17 +36,17 @@ def authorize(req):
     else:
         request_url = ['https://api.weixin.qq.com/sns/oauth2/access_token?appid=', settings.APPID, '&secret=', settings.APPSECRET, '&code=', code,  '&grant_type=authorization_code']
         request_url = ''.join(request_url)
-        print request_url
         stream = urllib2.urlopen(request_url)
-        res2 = json.loads(stream.read())
+        res2_str = stream.read();
+        res2_json = json.loads(res2_str)
         access_info = {
-            "access_token": res2['access_token'],
-            "expires_in": res2['expires_in'],
-            "refresh_token": res2['refresh_token'],
-            "openid": res2['openid'],
-            "scope": res2['scope']
+            "access_token": res2_json['access_token'],
+            "expires_in": res2_json['expires_in'],
+            "refresh_token": res2_json['refresh_token'],
+            "openid": res2_json['openid'],
+            "scope": res2_json['scope']
         }
-        return HttpResponse(access_info)
+        return HttpResponse(res2_str)
 
 # check signature
 def checksignature(req):
