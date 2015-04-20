ece2088dadbda05fb3ade568539a77585d5408c5
sy沈毅 <yi.shen@ctrip.com>
2015-04-20 16:10:20 +0800

    set /build to deploy /res

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: "./build/",
         filename: "app.bundle.js",
-        publicPath: './'
+        publicPath: '/res/'
     },
     module: {
         loaders: [
