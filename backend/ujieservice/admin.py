60b888918cd9d6485a16c5379f779caf9d142327
sy沈毅 <yi.shen@ctrip.com>
2015-04-29 18:07:11 +0800

    add User model to admin site

--- backend/ujieservice/admin.py
+++ backend/ujieservice/admin.py
@@ -1,18 +1,5 @@
 from django.contrib import admin
 # from django.contrib.auth.models import User
 from ujieservice.models import Order, User
-from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
-
-# class UserProfileInline(admin.TabularInline):
-#     model = Profile
-#
-#
-# class UserAdmin(DjangoUserAdmin):
-#     inlines = (UserProfileInline,)
-#
-#
-# admin.site.unregister(User)
-# admin.site.register(User, UserAdmin)
-admin.site.register([Order])
+admin.site.register([User, Order])
