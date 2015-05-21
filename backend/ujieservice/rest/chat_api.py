f53873b24ac4288f0f537465fdee7fa71d8185bb
shenyi <sy890622@gmail.com>
2015-05-22 00:15:09 +0800

    remove import igt

--- backend/ujieservice/rest/chat_api.py
+++ backend/ujieservice/rest/chat_api.py
@@ -1,9 +1,5 @@
 # coding:utf-8
 import json
-from igetui.template.igt_transmission_template import TransmissionTemplate
-from igt_push import IGeTui
-from igetui.igt_message import IGtSingleMessage
-from igetui.igt_target import *
 import requests
 import base64
 
@@ -50,24 +46,3 @@ class Send(APIView):
                 return Response(status=status.HTTP_400_BAD_REQUEST)
         except UserModel.DoesNotExist as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    # def _sendMsg(self, target):
-    #     template = TransmissionTemplate()
-    #     template.transmissionType = 1
-    #     template.appId = settings.IGT_APPID
-    #     template.appKey = settings.IGT_APPKEY
-    #     template.transmissionContent = target["msg"]
-    #     template.setPushInfo("PLAY", 1, target["msg"], "", "", "", "", "")
-    #
-    #     HOST = 'http://sdk.open.api.igexin.com/apiex.htm'
-    #     push = IGeTui(HOST, settings.IGT_APPKEY, settings.IGT_MASTERSECRET)
-    #     message = IGtSingleMessage()
-    #     message.isOffline = True
-    #     message.offlineExpireTime = 1000 * 3600 * 12
-    #     message.data = template
-    #     message.pushNetWorkType = 0 #设置是否根据WIFI推送消息，1为wifi推送，0为不限制推送
-    #     gt_target = Target()
-    #     gt_target.appId = settings.IGT_APPID
-    #     gt_target.clientId = target["user"].gt_clientid
-    #     ret = push.pushMessageToSingle(message, gt_target)
-    #     print ret
